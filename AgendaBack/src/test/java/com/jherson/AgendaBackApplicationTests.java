3c04fb28a2c32cc3d04a1928e9bd7e3027fd192d
Jherson <Jherson@DESKTOP-QJ1U8VR>
2021-09-05 02:22:37 -0500

    Inicio

---
+++ AgendaBack/src/test/java/com/jherson/AgendaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jherson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
